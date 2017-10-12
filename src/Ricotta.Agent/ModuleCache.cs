019248f34d1c779e4ef1b4d707be7f6e567ba79b
pondes <salil1011@gmail.com>
2017-10-12 22:04:34 +0100

    -

---
+++ src/Ricotta.Agent/ModuleCache.cs
@@ -0,0 +1,86 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Loader;
+using System.Text;
+
+namespace Ricotta.Agent
+{
+    public class ModuleCache
+    {
+        private string _moduleCachePath;
+        private ModuleRepository _moduleRepository;
+        private Dictionary<string, Assembly> _assemblies;   // <ModuleName, Assembly>
+
+        public ModuleCache(string moduleCachePath, ModuleRepository moduleRepository)
+        {
+            _moduleCachePath = moduleCachePath;
+            _moduleRepository = moduleRepository;
+            _assemblies = new Dictionary<string, Assembly>();
+        }
+
+        public bool ModuleLoaded(string moduleName)
+        {
+            return _assemblies.ContainsKey(moduleName);
+        }
+
+        public bool LoadModule(string moduleName)
+        {
+            var exists = _moduleRepository.ExistsLocally(moduleName);
+            if (!exists)
+            {
+                exists = _moduleRepository.Download(moduleName);
+            }
+            if (exists)
+            {
+                var modulePackagePath = _moduleRepository.GetModuleLocalPath(moduleName);
+                ExtractModulePackage(modulePackagePath);
+                var moduleDllFilename = $"Ricotta.Modules.{moduleName}.dll";
+                var moduleDllFilePath = Path.Combine(_moduleCachePath, moduleName, moduleDllFilename);
+                var moduleAssembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(moduleDllFilePath);
+                _assemblies.Add(moduleName, moduleAssembly);
+                return true;
+            }
+            return false;
+        }
+
+        public void Invoke(string moduleName, string methodName, object[] arguments)
+        {
+            var exists = ModuleLoaded(moduleName);
+            if (!exists)
+            {
+                exists = LoadModule(moduleName);
+            }
+            if (!exists)
+            {
+                throw new Exception($"Module {moduleName} does not exist");
+            }
+            var assembly = _assemblies[moduleName];
+            Invoke(assembly, moduleName, methodName, arguments);
+        }
+
+        private void Invoke(Assembly assembly, string moduleName, string methodName, object[] arguments)
+        {
+            var moduleClass = assembly.GetType($"Ricotta.Modules.{moduleName}");
+            object[] constructorArgs = null;
+            if (moduleName == "Package")
+            {
+                constructorArgs = new object[] { Log.Logger, null };    // TODO: Logger and FileRepository
+            }
+            else
+            {
+                constructorArgs = new object[] { Log.Logger };
+            }
+            var instance = Activator.CreateInstance(moduleClass, constructorArgs);
+            var method = moduleClass.GetMethod(methodName);
+            method.Invoke(instance, arguments);
+        }
+
+        private void ExtractModulePackage(string packagePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Ricotta.Agent/ModuleRepository.cs
@@ -0,0 +1,35 @@
+﻿using Ricotta.Transport;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ricotta.Agent
+{
+    public class ModuleRepository
+    {
+        public ModuleRepository(string repositoryPath, Client client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetModuleLocalPath(string moduleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ExistsLocally(string moduleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ExistsOnServer(string moduleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Download(string moduleName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
